b64561397aff7d98d3faec154b22ad1ffc2179d6
Divya <rachuridivya93@gmail.com>
2025-04-22 22:17:17 +0530

    integer values added

--- src/main/java/com/hcl/hsbc/customer/CustomerMicroServiceApplication.java
+++ src/main/java/com/hcl/hsbc/customer/CustomerMicroServiceApplication.java
@@ -13,26 +13,24 @@ import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
 @SpringBootApplication
 public class CustomerMicroServiceApplication {
 
-<<<<<<< Updated upstream
+ Updated upstream
 //103 logit of jiragit
 public void m3(){
 //gjkjh}
-=======
+
 //jira 102
 public void m2(){
 //logic
 
 }
->>>>>>> Stashed changes
+ 
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerMicroServiceApplication.class, args);
 		
 		System.out.println("-----------customer main service------");
 
-String name="Divya";
-String place="hyd";
-String name2="Java";
+int r=30;
 
 	}
 }
